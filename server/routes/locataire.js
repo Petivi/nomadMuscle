c4d44e33b6d1a2e39cb026fdf52b2db04033a5ad
neskwik93 <eric.bauman@laposte.net>
2019-05-20 16:10:52 +0200

    locataire post

--- server/routes/locataire.js
+++ server/routes/locataire.js
@@ -2,19 +2,25 @@ const Locataire = require('./../models/locataire');
 
 module.exports = (app) => {
 
-  // app.post('/films', (req, res) => {
-  //   var film = new Film(req.body.film);
-  //   film.save().then(result => {
-  //     res.sendStatus(201);
-  //   })
-  // })
-
-
   app.get('/locataires', (req, res) => {
     Locataire.find({})
-    .then(locataires => {
-      res.send(locataires);
-    })
+      .then(locataires => {
+        res.send(locataires);
+      })
   })
+
+  app.get('/locataires/:id', (req, res) => {
+    Locataire.find({ _id: req.params.id })
+      .then(locataire => {
+        res.send(locataire);
+      })
+  })
+
+  app.post('/locataires', (req, res) => {
+    var locataire = new Locataire(req.body);
+    locataire.save().then(result => {
+      res.sendStatus(201);
+    });
+  });
 
 }
