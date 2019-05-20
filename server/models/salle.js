9a6d7402aa549cf16ab47458e696a3d6f8d3295f
neskwik93 <eric.bauman@laposte.net>
2019-05-20 16:15:32 +0200

    sais pas quoi mettre

--- server/models/salle.js
+++ server/models/salle.js
@@ -6,26 +6,27 @@ const SalleSchema = new Schema({
     idBailleur: { type: String },
     nom: { type: String },
     adresse: { type: String, required: true },
-    dimension: { type: Number, required: true},
+    dimension: { type: Number, required: true },
     equipements: [
-      { type: String}
+        { type: String }
     ],
     utilisateurMax: { type: Number, required: true },
-    disponibilite: { semaine: [
-      {type: Jour, required: true}
+    disponibilite: {
+        semaine: [
+            { type: Jour, required: true }
+        ],
+        mois: String,
+        exception: [Date]
+    },
+    service: [
+        { type: String }
     ],
-    mois: String,
-    exception: [Date]
-  },
-  service: [
-    { type: String }
-  ],
-  photo: [
-    { type: String }
-  ],
-  validationAuto:{ type: Boolean },
-  tarifHoraire: { type: Number },
-  pourcentageRemboursement: { type: Number }
+    photos: [
+        { type: String }
+    ],
+    validationAuto: { type: Boolean },
+    tarifHoraire: { type: Number },
+    pourcentageRemboursement: { type: Number }
 })
 
 
--- server/routes/salle.js
+++ server/routes/salle.js
@@ -2,34 +2,26 @@ const Salle = require('./../models/salle');
 
 module.exports = (app) => {
 
-  // app.post('/films', (req, res) => {
-  //   var film = new Film(req.body.film);
-  //   film.save().then(result => {
-  //     res.sendStatus(201);
-  //   })
-  // })
+    app.get('/salles', (req, res) => {
+        Salle.find({})
+            .then(salles => {
+                res.send(salles);
+            });
+    });
 
+    app.get('/salles/:id', (req, res) => {
+        Salle.find({ _id: req.params.id })
+            .then(salles => {
+                console.log(salles.length);
+                res.send(salles);
+            });
+    });
 
-  app.get('/salles', (req, res) => {
-    Salle.find({})
-    .then(salles => {
-      res.send(salles);
-    })
-  })
-
-  app.get('/salles/:id', (req, res) => {
-    Salle.find({_id: req.params.id})
-    .then(salles => {
-      console.log(salles.length);
-      res.send(salles);
-    })
-  })
-
-  app.post('/salles', (req, res) => {
-    var salle = new Salle(req.body);
-    salle.save().then(result => {
-      res.sendStatus(201);
-    })
-  })
+    app.post('/salles', (req, res) => {
+        var salle = new Salle(req.body);
+        salle.save().then(result => {
+            res.sendStatus(201);
+        });
+    });
 
 }
