9417f8cf57534433c042472767321adbb6212b24
neskwik93 <eric.bauman@laposte.net>
2019-05-20 13:50:06 +0200

    jour

---
+++ server/models/bailleurs.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BailleurSchema = new Schema({
+    nom: { type: String, required: true },
+    prenom: { type: String, required: true },
+    dateNaissance: { type: String, required: true },
+    adresse: { type: String, required: true },
+    pieceId: { type: String, required: true },
+})
+
+
+const Bailleur = mongoose.model('bailleur', BailleurSchema);
+
+
+module.exports = Bailleur;
---
+++ server/models/jour.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const JourSchema = new Schema({
+    titre: { type: String, required: true },
+    debut: Number,
+    fin: Number
+})
+
+const Jour = mongoose.model('jour', JourSchema);
+
+module.exports = Jour;
