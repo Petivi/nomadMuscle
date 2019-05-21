68ff12aae6c91a8c10170242214702639d85d51d
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-21 13:54:00 +0200

    Update locataire.js

--- server/models/locataire.js
+++ server/models/locataire.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 const LocataireSchema = new Schema({
     nom: { type: String, required: true },
     prenom: { type: String, required: true },
-    dateNaissance: { type: String, required: true },
+    dateNaissance: { type: Date, required: true },
     mail: { type: String, required: true },
     pieceId: { type: String, required: true },
     certificat: { type: String, required: true },
     solde: { type: Number, required: true },
-    password: { type: String, required: true }
     password: { type: String, required: true },
     token: { type: String }
 })
