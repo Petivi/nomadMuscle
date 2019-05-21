f2f5f46fca5fdff9deed9fa3c18ff9097e61187a
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-21 14:14:36 +0200

    Update login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,24 +9,6 @@ const {generateToken} = require('./../utils/generateToken');
 
 module.exports = (app) => {
 
-
-//   app.post('/login', (req, res) =>{
-//   var {mail, password} = req.body;
-//   connection.query("SELECT * FROM prof WHERE mail LIKE '"+ mail+"' AND password LIKE '" + password + "'", function (err, user, fields) {
-//     if (err) throw err;
-//     if(user.length > 0){
-//
-//
-//       generateToken(user[0].id)
-//       .then(token => {
-//         res.header('x-auth', token).send('PASSWORD_MATCH');
-//       });
-//     }else {
-//       res.status(200).send('USER_NOT_FOUND');
-//     }
-//   });
-// });
-
   app.post('/inscription', cryptPassword, (req, res) =>{
     var typeUser = req.body.type;
     var mail = req.body.data.mail;
