fe833a3e23fbbda54591035880dd1d16ff689bde
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-22 14:01:11 +0200

    Update salle.js

--- server/routes/salle.js
+++ server/routes/salle.js
@@ -3,17 +3,34 @@ const {authenticate} = require('./../middleware/authenticate');
 
 module.exports = (app) => {
 
-    app.get('/salles', (req, res) => {
+    app.get('/salles', authenticate, (req, res) => {
+      if(req.body.type == 'bailleur'){
+        Salle.find({ idBailleur: req.body.user_id })
+        .then(salles => {
+          if(salles.length != 0){
+            res.send({response:salles});
+          }else {
+            res.send({response: 'NO_ITEMS_FOUND'});
+          }
+        });
+      }else if (req.body.type == 'locataire') {
         Salle.find({})
-            .then(salles => {
-                res.send({'response':salles});
-            });
+        .then(salles => {
+          if(salles.length != 0){
+            res.send({response:salles});
+          }else {
+            res.send({response: 'NO_ITEMS_FOUND'});
+          }
+        });
+      }else {
+        res.send({error:'ERR_TYPE_INVALID'});
+      }
     });
 
     app.get('/salles/:id', (req, res) => {
         Salle.find({ _id: req.params.id })
             .then(salles => {
-                res.send({'reponse': salles});
+                res.send({reponse: salles});
             });
     });
 
@@ -21,7 +38,7 @@ module.exports = (app) => {
         if(req.body.type == "bailleur"){
           var salle = new Salle(req.body.data);
           salle.save().then(result => {
-            res.status(201).send({'response':'true'});
+            res.status(201).send({response:'true'});
           });
         }else {
           res.send({'error':'ERR_TYPE_INVALID'});
