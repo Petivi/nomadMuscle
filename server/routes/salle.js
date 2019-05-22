066f3a4404e25929bf4017f77f3c21ca599ccb4a
Neskwik93 <eric.bauman@laposte.net>
2019-05-22 13:50:09 +0200

    .

--- server/routes/salle.js
+++ server/routes/salle.js
@@ -6,14 +6,14 @@ module.exports = (app) => {
     app.get('/salles', (req, res) => {
         Salle.find({})
             .then(salles => {
-                res.send({'response':salles});
+                res.send({ response: salles });
             });
     });
 
     app.get('/salles/:id', (req, res) => {
         Salle.find({ _id: req.params.id })
             .then(salles => {
-                res.send({'reponse': salles});
+                res.send({ 'reponse': salles });
             });
     });
 
@@ -22,10 +22,10 @@ module.exports = (app) => {
             req.body.data.idBailleur = req.body.user_id;
             var salle = new Salle(req.body.data);
             salle.save().then(result => {
-                res.status(201).send({response: 'created'});
+                res.status(201).send({ response: 'created' });
             });
         } else {
-            res.status(500).send({error: 'ERR_TYPE_INVALID'});
+            res.status(500).send({ error: 'ERR_TYPE_INVALID' });
         }
     });
 
