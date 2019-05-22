1c895bcabfc86cbcc3bb01bef0bed7e4a47102c4
Neskwik93 <eric.bauman@laposte.net>
2019-05-22 14:32:27 +0200

    find room locataire

--- nomadMuscle/src/app/find-room/find-room.component.ts
+++ nomadMuscle/src/app/find-room/find-room.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppService } from '../app.service';
+
+import { Salle } from '../models/app.model';
+
 @Component({
-  selector: 'app-find-room',
-  templateUrl: './find-room.component.html',
-  styleUrls: ['./find-room.component.scss']
+	selector: 'app-find-room',
+	templateUrl: './find-room.component.html',
+	styleUrls: ['./find-room.component.scss']
 })
 export class FindRoomComponent implements OnInit {
+	ttSalle: Salle[];
+	
+	constructor(private _appService: AppService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this._appService.get('salles').then((salles: any) => {
+			this.ttSalle = salles.response;
+			console.log(this.ttSalle)
+		});
+	}
 
 }
