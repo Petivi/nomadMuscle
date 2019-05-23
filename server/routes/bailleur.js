38de843400fd9816707b89600222f11883f77443
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-23 14:59:06 +0200

    Update bailleur.js

--- server/routes/bailleur.js
+++ server/routes/bailleur.js
@@ -2,19 +2,17 @@ const Bailleur = require('./../models/bailleur');
 
 module.exports = (app) => {
 
-    app.get('/bailleurs', (req, res) => {
-        Bailleur.find({})
-            .then(bailleurs => {
-                res.send({ reponse: bailleurs });
-            });
+    app.get('/bailleurs', authenticate, (req, res) => {
+      if(req.body.type == "bailleur"){
+        Bailleur.find({ _id: req.body.user_id })
+        .then(bailleurs => {
+          res.send({ reponse: bailleurs });
+        });
+      }else {
+        res.send({error:"ERR_TYPE_INVALID"});
+      }
     });
 
-    app.get('/bailleurs/:id', (req, res) => {
-        Bailleur.find({ _id: req.params.id })
-            .then(bailleur => {
-                res.send({ response: bailleur });
-            });
-    });
 
     app.post('/bailleurs', (req, res) => {
         let bailleur = new Bailleur(req.body.data);
