80b20bbccc6ea2765a6f0e1f6968a295c4388306
Lea <lea.rerolle@gmail.com>
2019-05-23 10:16:33 +0200

    faut commit

--- nomadMuscle/src/app/register-room/register-room.component.ts
+++ nomadMuscle/src/app/register-room/register-room.component.ts
@@ -10,12 +10,39 @@ import { AppService } from '../app.service';
     styleUrls: ['./register-room.component.scss']
 })
 export class RegisterRoomComponent implements OnInit {
-    salle: Salle = new Salle({});
+    salle: Salle = new Salle({
+        disponibilite: {
+            semaine: [
+                { titre: 'Lundi', debut: 0, fin: 0 },
+                { titre: 'Mardi', debut: 0, fin: 0 },
+                { titre: 'Mercredi', debut: 0, fin: 0 },
+                { titre: 'Jeudi', debut: 0, fin: 0 },
+                { titre: 'Vendredi', debut: 0, fin: 0 },
+                { titre: 'Samedi', debut: 0, fin: 0 },
+                { titre: 'Dimanche', debut: 0, fin: 0 },
+            ]
+        }
+    });
     selectedDate: Date;
+    times: any[] = [];
 
     constructor(private _appService: AppService) { }
 
     ngOnInit() {
+        this.getHalfHours();
+    }
+
+    getHalfHours() {
+        var x = 30; //minutes interval
+        // var times = []; // time array
+        var tt = 0; // start time
+
+        for (var i = 0; tt < 24 * 60; i++) {
+            var hh = Math.floor(tt / 60); // getting hours of day in 0-24 format
+            var mm = (tt % 60); // getting minutes of the hour in 0-55 format
+            this.times[i] = ("0" + (hh)).slice(-2) + ':' + ("0" + mm).slice(-2); // pushing data in array in [00:00 - 12:00 AM/PM format]
+            tt = tt + x;
+        }
     }
 
     addSalle() {
@@ -38,7 +65,7 @@ export class RegisterRoomComponent implements OnInit {
     }
 
     addException() {
-        this.salle.disponibilite = this.salle.disponibilite ? this.salle.disponibilite : {semaine: [], exception: []};
+        this.salle.disponibilite = this.salle.disponibilite ? this.salle.disponibilite : { semaine: [], exception: [] };
         this.salle.disponibilite.exception.push(this.selectedDate);
         console.log(this.salle)
     }
