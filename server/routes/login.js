e427982a2f4a7199ef449239d2695962288c8233
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-23 17:16:12 +0200

    Update login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -47,7 +47,6 @@ module.exports = (app) => {
         var password = req.body.data.password;
         var user_hashed_password = "";
         var user_id = "";
-        var user_pieceId = "";
 
         if (typeUser == "locataire") {
             Locataire.find({ mail: mail })
@@ -55,8 +54,7 @@ module.exports = (app) => {
                     if (locataire[0]) {
                         user_hashed_password = locataire[0].password;
                         user_id = locataire[0]._id;
-                        user_pieceId = locataire[0].pieceId;
-                        password_verify(password, user_hashed_password, user_id, typeUser, user_pieceId, res);
+                        password_verify(password, user_hashed_password, user_id, typeUser, res);
                     } else {
                         res.status(200).send({ error: 'ERR_USER_UNKNOWN' });
                     }
@@ -67,8 +65,7 @@ module.exports = (app) => {
                     if (bailleur[0]) {
                         user_hashed_password = bailleur[0].password;
                         user_id = bailleur[0]._id;
-                        user_pieceId = bailleur[0].pieceId;
-                        password_verify(password, user_hashed_password, user_id, typeUser, user_pieceId, res);
+                        password_verify(password, user_hashed_password, user_id, typeUser, res);
                     } else {
                         res.status(200).send({ error: 'ERR_USER_UNKNOWN' });
                     }
@@ -79,12 +76,12 @@ module.exports = (app) => {
     });
 
 
-    function password_verify(password, user_hashed_password, user_id, typeUser, user_pieceId, res) {
+    function password_verify(password, user_hashed_password, user_id, typeUser, res) {
         bcrypt.compare(password, user_hashed_password, (err, result) => {
             if (result) {
                 generateToken(user_id, typeUser)
                     .then(token => {
-                        res.header('x-auth', token).send({ response: 'PASSWORD_MATCH', token: token, user_pieceId: user_pieceId });
+                        res.header('x-auth', token).send({ response: 'PASSWORD_MATCH', token: token });
                     });
             } else {
                 res.status(200).send({ error: 'PASSWORD_NOT_MATCH' });
