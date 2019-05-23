5d30fc977adf4bb9c407c0952ec3a4656c56d0f4
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-23 11:17:30 +0200

    Crediter solde au locataire

--- server/routes/locataire.js
+++ server/routes/locataire.js
@@ -1,4 +1,5 @@
 const Locataire = require('./../models/locataire');
+const { authenticate } = require('./../middleware/authenticate');
 
 module.exports = (app) => {
 
@@ -17,12 +18,29 @@ module.exports = (app) => {
   })
 
   app.post('/locataires', (req, res) => {
-    console.log(req.body)
     var locataire = new Locataire(req.body.data);
     locataire.solde = 0;
     locataire.save().then(result => {
       res.status(201).send({response: 'created'});
     });
+  });
+
+
+  app.patch('/locataires/solde', authenticate, (req, res) => {
+    var user_id = req.body.user_id
+    if(req.body.type == "locataire"){
+      var add_solde = req.body.data.solde;
+      Locataire.find({ _id: user_id })
+        .then(locataire => {
+          var user_solde = locataire[0].solde;
+          var new_solde = user_solde + add_solde;
+          Locataire.findOneAndUpdate({_id:user_id}, {solde: new_solde}).then(locataire => {
+            res.send({response:"BALANCE_EDITED"});
+          });
+        })
+    }else {
+      res.send({error:"ERR_TYPE_INVALID"});
+    }
   });
 
 }
