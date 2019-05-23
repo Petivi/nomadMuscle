64b8df885c6f43b14f349c078a625d2ef716d1f8
Petivi <33059675+Petivi@users.noreply.github.com>
2019-05-23 13:10:07 +0200

    Update fonction.js

--- server/utils/fonction.js
+++ server/utils/fonction.js
@@ -93,3 +93,14 @@ getSalleLocataire = (transaction, salle = null, locataire = null) => {
         });
     });
   }
+
+  module.exports.getCustomHeure = (heure) => {
+
+    if(heure%2 == 1){ // avec demie heure
+      var newHeure = (heure-1)/2;
+      return newHeure.toString().padStart(2, "0")+":30";
+    }else { // sans demie heure
+      return (heure/2).toString().padStart(2, "0")+":00";
+    }
+
+  }
