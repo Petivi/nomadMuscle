2612a07819dd0cfea7265c103959d98ae223c076
Neskwik93 <eric.bauman@laposte.net>
2019-05-23 16:45:23 +0200

    .

--- nomadMuscle/src/app/models/app.model.ts
+++ nomadMuscle/src/app/models/app.model.ts
@@ -81,3 +81,10 @@ export class Transaction {
         }
     }
 }
+
+export class ImageSnippet {
+    pending: boolean = false;
+    status: string = 'init';
+  
+    constructor(public src: string, public file: File) {}
+  }
